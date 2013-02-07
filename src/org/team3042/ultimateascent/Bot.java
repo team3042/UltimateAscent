22fa3068ef576f777743bd1c65128fe4fe983d38
Jeremy Koletar <jjkoletar@gmail.com>
2013-02-07 16:10:42 -0600

    Flesh out actuators, add in more specific drive code

--- src/org/team3042/ultimateascent/Bot.java
+++ src/org/team3042/ultimateascent/Bot.java
@@ -1,5 +1,6 @@
 package org.team3042.ultimateascent;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
@@ -15,9 +16,18 @@ public final class Bot {
         public static final Jaguar LEFT_JAG = new Jaguar(1);
         public static final Jaguar RIGHT_JAG = new Jaguar(2);
         public static final RobotDrive DRIVETRAIN = new RobotDrive(LEFT_JAG, RIGHT_JAG);
+        public static final Jaguar LEFT_ACTUATOR = new Jaguar(3);
+        public static final Jaguar RIGHT_ACTUATOR = new Jaguar(4);
     }
     public static final class Input {
         private Input() {}
         public static final Joystick driver = new Joystick(1);
+        public static final Joystick gunner = new Joystick(2);
+        //A_L_S is true when circuit is closed, and switch is not triggered, false when switch triggered or circuit open
+        public static final DigitalInput ACTUATOR_LIMIT_SWITCH = new DigitalInput(14);
+    }
+    public static final class Constants {
+        private Constants() {}
+        public static final double ACTUATOR_COEFFICIENT = 0.5;
     }
 }
--- src/org/team3042/ultimateascent/EastviewRobot.java
+++ src/org/team3042/ultimateascent/EastviewRobot.java
@@ -2,6 +2,7 @@ package org.team3042.ultimateascent;
 
 import edu.wpi.first.wpilibj.SimpleRobot;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * Main Robot Class
@@ -9,25 +10,58 @@ import edu.wpi.first.wpilibj.Timer;
  */
 public class EastviewRobot extends SimpleRobot {
     public void autonomous() {
-
     }
 
     public void operatorControl() {
         Bot.Hardware.DRIVETRAIN.setSafetyEnabled(true);
         while (isOperatorControl() && isEnabled()) { //Teleop Loop, executed while robot runs
             //Drivetrain
-            Bot.Hardware.DRIVETRAIN.arcadeDrive(scale(Bot.Input.driver.getY()), scale(Bot.Input.driver.getTwist()));
-
+            if (Math.abs(Bot.Input.driver.getY()) < 0.1) {
+                //Do a twist move
+                SmartDashboard.putBoolean("Ran twist move", true);
+                SmartDashboard.putBoolean("Ran travel move", false);
+                Bot.Hardware.DRIVETRAIN.arcadeDrive(0.0, scale(Math.abs(Bot.Input.driver.getTwist()) > 0.3 ? -Bot.Input.driver.getTwist() : 0), true);
+            } else {
+                //Do only forward/back
+                SmartDashboard.putBoolean("Ran twist move", false);
+                SmartDashboard.putBoolean("Ran travel move", true);
+                Bot.Hardware.DRIVETRAIN.arcadeDrive(scale(Bot.Input.driver.getY()), 0.0, true);
+            }
+            SmartDashboard.putNumber("Joy Y", scale(Bot.Input.driver.getY()));
+            SmartDashboard.putNumber("Scale Value", ((-Bot.Input.driver.getThrottle() + 1) / 2));
+            SmartDashboard.putNumber("Twist", Bot.Input.driver.getTwist());
+            //Actuators
+            SmartDashboard.putNumber("Actuator In", Bot.Input.gunner.getY());
+            SmartDashboard.putNumber("Actuator Scaled", scaleActuators(Bot.Input.gunner.getY()));
+            SmartDashboard.putBoolean("Actuator Limit Switch", Bot.Input.ACTUATOR_LIMIT_SWITCH.get());
+            if (Bot.Input.ACTUATOR_LIMIT_SWITCH.get()) {
+                Bot.Hardware.LEFT_ACTUATOR.set(scaleActuators(Bot.Input.gunner.getY()));
+                Bot.Hardware.RIGHT_ACTUATOR.set(scaleActuators(Bot.Input.gunner.getY()));
+            } else {
+                Bot.Hardware.LEFT_ACTUATOR.set(0.0);
+                Bot.Hardware.RIGHT_ACTUATOR.set(0.0);
+            }
             Timer.delay(0.01);
         }
     }
 
     private double scale(double in) {
         //Throttle value is 1.0 through -1.0, so we gotta scale it to be between 1.0 and 0.0
-        return ((Bot.Input.driver.getThrottle() + 1) / 2) * in;
+        return ((-Bot.Input.driver.getThrottle() + 1) / 2) * in;
+    }
+
+    private double scaleActuators(double in) {
+        return in * Bot.Constants.ACTUATOR_COEFFICIENT;
     }
 
     public void test() {
-
+        while (isTest() && isEnabled()) {
+            Bot.Hardware.DRIVETRAIN.arcadeDrive(0.5, 0.0);
+            Bot.Hardware.LEFT_JAG.set(0.5);
+            Bot.Hardware.RIGHT_JAG.set(0.5);
+            Timer.delay(0.01);
+        }
+        Bot.Hardware.LEFT_JAG.set(0);
+        Bot.Hardware.RIGHT_JAG.set(0);
     }
 }
