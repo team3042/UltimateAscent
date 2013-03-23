601032a08f25f778bd4909831a6f0af93a6268aa
Jeremy Koletar <jjkoletar@gmail.com>
2013-03-23 13:05:31 -0500

    Commit latest version. Added light control, etc

--- src/org/team3042/ultimateascent/Bot.java
+++ src/org/team3042/ultimateascent/Bot.java
@@ -1,9 +1,6 @@
 package org.team3042.ultimateascent;
 
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Jaguar;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.*;
 
 /**
  * Class full of various constants
@@ -16,18 +13,24 @@ public final class Bot {
         public static final Jaguar LEFT_JAG = new Jaguar(1);
         public static final Jaguar RIGHT_JAG = new Jaguar(2);
         public static final RobotDrive DRIVETRAIN = new RobotDrive(LEFT_JAG, RIGHT_JAG);
-        public static final Jaguar LEFT_ACTUATOR = new Jaguar(3);
-        public static final Jaguar RIGHT_ACTUATOR = new Jaguar(4);
+        public static final Jaguar LEFT_ACTUATOR = new Jaguar(4);
+        public static final Jaguar RIGHT_ACTUATOR = new Jaguar(3);
+        public static final Relay LEFT_LIGHTS = new Relay(5);
+        public static final Relay RIGHT_LIGHTS = new Relay(8);
+        public static final Relay BAR_ASSIST = new Relay(7);
     }
     public static final class Input {
         private Input() {}
         public static final Joystick driver = new Joystick(1);
         public static final Joystick gunner = new Joystick(2);
         //A_L_S is true when circuit is closed, and switch is not triggered, false when switch triggered or circuit open
-        public static final DigitalInput ACTUATOR_LIMIT_SWITCH = new DigitalInput(14);
+        public static final DigitalInput RIGHT_ACTUATOR_LIMIT_SWITCH = new DigitalInput(14);
+        public static final DigitalInput LEFT_ACTUATOR_LIMIT_SWITCH = new DigitalInput(13);
     }
     public static final class Constants {
         private Constants() {}
         public static final double ACTUATOR_COEFFICIENT = 1.0;
+        public static final int LEFT_ACTUATOR_OVERRIDE_BUTTON = 8;
+        public static final int RIGHT_ACTUATOR_OVERRIDE_BUTTON = 9;
     }
 }
--- src/org/team3042/ultimateascent/EastviewRobot.java
+++ src/org/team3042/ultimateascent/EastviewRobot.java
@@ -1,14 +1,24 @@
 package org.team3042.ultimateascent;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.SimpleRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * Main Robot Class
+ *
  * @author jjkoletar
  */
 public class EastviewRobot extends SimpleRobot {
+    private boolean leftBlinkInputLock = false;
+    private double leftBlinkTimer = -1;
+    private boolean rightBlinkInputLock = false;
+    private double rightBlinkTimer = -1;
+    private int leftLimitSignal = -1;
+    private int rightLimitSignal = -1;
+
     public void robotInit() {
         super.robotInit();
         System.out.println("This is the Competition Code used by Team 3042 for the FRC Ultimate Ascent Game");
@@ -40,17 +50,99 @@ public class EastviewRobot extends SimpleRobot {
             //Actuators
             SmartDashboard.putNumber("Actuator In", Bot.Input.gunner.getY());
             SmartDashboard.putNumber("Actuator Scaled", scaleActuators(Bot.Input.gunner.getY()));
-            SmartDashboard.putBoolean("Actuator Limit Switch", Bot.Input.ACTUATOR_LIMIT_SWITCH.get());
-            if (!Bot.Input.ACTUATOR_LIMIT_SWITCH.get()) {
-               // Bot.Hardware.LEFT_ACTUATOR.set(scaleActuators(Bot.Input.gunner.getY()));
-                Bot.Hardware.LEFT_ACTUATOR.set(0.0);
-                Bot.Hardware.RIGHT_ACTUATOR.set(scaleActuators(Bot.Input.gunner.getY()));
-            } else {
-                Bot.Hardware.LEFT_ACTUATOR.set(0.0);
+            SmartDashboard.putBoolean("Right Actuator Limit Switch", Bot.Input.RIGHT_ACTUATOR_LIMIT_SWITCH.get());
+            SmartDashboard.putBoolean("Left Actuator Limit Switch", Bot.Input.LEFT_ACTUATOR_LIMIT_SWITCH.get());
+            if (Bot.Input.gunner.getRawButton(Bot.Constants.LEFT_ACTUATOR_OVERRIDE_BUTTON)) {
+                SmartDashboard.putString("Actuator Style", "Left");
+                Bot.Hardware.LEFT_ACTUATOR.set(limitSwitch(Bot.Input.LEFT_ACTUATOR_LIMIT_SWITCH, scaleActuators(Bot.Input.gunner.getY())));
                 Bot.Hardware.RIGHT_ACTUATOR.set(0.0);
+            } else if (Bot.Input.gunner.getRawButton(Bot.Constants.RIGHT_ACTUATOR_OVERRIDE_BUTTON)) {
+                SmartDashboard.putString("Actuator Style", "Right");
+                Bot.Hardware.LEFT_ACTUATOR.set(0.0);
+                Bot.Hardware.RIGHT_ACTUATOR.set(limitSwitch(Bot.Input.RIGHT_ACTUATOR_LIMIT_SWITCH, scaleActuators(Bot.Input.gunner.getY())));
+            } else {
+                SmartDashboard.putString("Actuator Style", "Both");
+                Bot.Hardware.LEFT_ACTUATOR.set(limitSwitch(Bot.Input.LEFT_ACTUATOR_LIMIT_SWITCH, scaleActuators(Bot.Input.gunner.getY())));
+                Bot.Hardware.RIGHT_ACTUATOR.set(limitSwitch(Bot.Input.RIGHT_ACTUATOR_LIMIT_SWITCH, scaleActuators(Bot.Input.gunner.getY())));
+            }
+            if (!leftBlinkInputLock && Bot.Input.gunner.getRawButton(11)) {
+                leftBlinkTimer = leftBlinkTimer >= 0 ? -1 : 0;
+                leftBlinkInputLock = true;
+            }
+            if (leftBlinkInputLock && !Bot.Input.gunner.getRawButton(11)) {
+                leftBlinkInputLock = false;
+            }
+            if (!rightBlinkInputLock && Bot.Input.gunner.getRawButton(6)) {
+                rightBlinkTimer = rightBlinkTimer >= 0 ? -1 : 0;
+                rightBlinkInputLock = true;
+            }
+            if (rightBlinkInputLock && !Bot.Input.gunner.getRawButton(6)) {
+                rightBlinkInputLock = false;
+            }
+            if (leftBlinkTimer >= 0) {
+                leftBlinkTimer += 0.01;
+                if (leftBlinkTimer >= 0.8) {
+                    toggle(Bot.Hardware.LEFT_LIGHTS);
+                    leftBlinkTimer = 0;
+                }
+            }
+            if (Bot.Input.gunner.getRawButton(10)) {
+                Bot.Hardware.LEFT_LIGHTS.set(Relay.Value.kForward);
+            } else if (leftBlinkTimer < 0) {
+                Bot.Hardware.LEFT_LIGHTS.set(Relay.Value.kOff);
+            }
+            if (rightBlinkTimer >= 0) {
+                rightBlinkTimer += 0.01;
+                if (rightBlinkTimer >= 0.8) {
+                    toggle(Bot.Hardware.RIGHT_LIGHTS);
+                    rightBlinkTimer = 0;
+                }
+            }
+            if (Bot.Input.gunner.getRawButton(7)) {
+                Bot.Hardware.RIGHT_LIGHTS.set(Relay.Value.kForward);
+            } else if (rightBlinkTimer < 0) {
+                Bot.Hardware.RIGHT_LIGHTS.set(Relay.Value.kOff);
+            }
+            if (Bot.Input.RIGHT_ACTUATOR_LIMIT_SWITCH.get()) {
+                leftLimitSignal++;
+                if (leftLimitSignal == 30) {
+                    toggle(Bot.Hardware.LEFT_LIGHTS);
+                    leftLimitSignal = 0;
+                }
+            } else {
+                if (leftBlinkTimer == -1 && !Bot.Input.gunner.getRawButton(10)) {
+                    Bot.Hardware.LEFT_LIGHTS.set(Relay.Value.kOff);
+                }
+            }
+            if (Bot.Input.LEFT_ACTUATOR_LIMIT_SWITCH.get()) {
+                rightLimitSignal++;
+                if (rightLimitSignal == 30) {
+                    toggle(Bot.Hardware.RIGHT_LIGHTS);
+                    rightLimitSignal = 0;
+                }
+            } else {
+                if (rightBlinkTimer == -1 && !Bot.Input.gunner.getRawButton(7)) {
+                    Bot.Hardware.RIGHT_LIGHTS.set(Relay.Value.kOff);
+                }
+            }
+            if (Bot.Input.gunner.getRawButton(3)) {
+                Bot.Hardware.BAR_ASSIST.set(Relay.Value.kForward);
+            } else if (Bot.Input.gunner.getRawButton(2)) {
+                Bot.Hardware.BAR_ASSIST.set(Relay.Value.kReverse);
+            } else {
+                Bot.Hardware.BAR_ASSIST.set(Relay.Value.kOff);
             }
             Timer.delay(0.01);
         }
+    }
+
+    private void toggle(Relay light) {
+        light.set(light.get().equals(Relay.Value.kOff) ? Relay.Value.kForward : Relay.Value.kOff);
+    }
+
+    private double limitSwitch(DigitalInput theSwitch, double value) {
+        return value; //TODO: Disabled for KBII
+        //return theSwitch.get() ? (value < 0 ? value : 0) : value;
     }
 
     private double scale(double in) {
